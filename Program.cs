eba4ad4b79b0b65f79249a291c1f6e6a44eb3116
magnus <magnus.fiske@iths.se>
2022-09-06 15:27:39 +0200

    uppdaterade kommentarer

--- Program.cs
+++ Program.cs
@@ -14,8 +14,11 @@ namespace kalkylator
 
             cal1.NumberOne = cal1.GetNumber();
             cal1.NumberTwo = cal1.GetNumber();
+            //En metod av klassen cal1 för att hämta båda talen
             cal1.Op = cal1.GetOperator();
+            //hämtar vilket räknesätt som ska användas
             cal1.Answer = cal1.Compute(cal1.NumberOne, cal1.NumberTwo, cal1.Op);
+            //gör själva beräkningen
 
             Console.WriteLine("{0} {1} {2} = {3}", cal1.NumberOne, cal1.Op, cal1.NumberTwo, cal1.Answer);
 
